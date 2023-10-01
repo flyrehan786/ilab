40ce91192b645202ae6b1ecab1db8dd99b405dff
Rehan Shah <flyrehan786@gmail.com>
2023-10-02 01:32:51 +0500

    adding mysql service.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log('Server')
+const { check_connection } = require('./services/mysql');
+const express = require("express");
+
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+    check_connection();
+});
+
+module.exports = server;
