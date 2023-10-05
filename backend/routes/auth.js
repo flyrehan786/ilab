6adc61c8d66e73a6f6928519387f0989589321b4
Rehan Shah <flyrehan786@gmail.com>
2023-10-05 23:46:20 +0500

    code refactoring.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -19,8 +19,8 @@ router.post('/', async (req, res) => {
 
 function validate(req) {
   const schema = {
-    username: Joi.string().min(5).max(255).required(),
-    password: Joi.string().min(5).max(255).required()
+    username: Joi.string().min(2).max(255).required(),
+    password: Joi.string().min(2).max(1024).required()
   };
   return Joi.validate(req, schema);
 }
