353cacaf732bde0180ad6f4b1ff4929945565e41
Rehan Shah <flyrehan786@gmail.com>
2023-10-10 00:08:21 +0500

    updating doctor model.

--- backend/models/doctor.js
+++ backend/models/doctor.js
@@ -44,15 +44,14 @@ async function findAll() {
 
 async function saveDoctor(newDoctor) {
     return new Promise((resolve, reject) => {
-        db.execute(`INSERT INTO doctors VALUES(default, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW(), 1, 1)`,
+        db.execute(`INSERT INTO doctors VALUES(default, ?, ?, ?, ?, ?, ?, NOW(), NOW())`,
             [
-                newDoctor.first_name,
-                newDoctor.last_name,
-                newDoctor.email,
-                newDoctor.username,
-                newDoctor.password,
-                newDoctor.is_admin,
-                newDoctor.status
+                newDoctor.full_name,
+                newDoctor.gender,
+                newDoctor.contact_number,
+                newDoctor.email_address,
+                newDoctor.address,
+                newDoctor.specialization,
             ], (err, result) => {
                 if (err) reject(err);
                 db.execute(`SELECT id FROM doctors WHERE id = LAST_INSERT_ID();`, (err, result) => {
@@ -79,19 +78,14 @@ async function findDoctor(id) {
 
 async function updateDoctor(id, updatedDoctor) {
     return new Promise((resolve, reject) => {
-        db.execute('Update students SET registration_no=?,first_name=?, last_name=?, gender=?, cnic=?, age=?, father_name=?, father_cnic=?, post_office=?, tehsil=?, district=? WHERE id=?;',
+        db.execute('Update students SET full_name=?,gender=?, contact_number=?, email_address=?, address=?, specialization=? WHERE id=?;',
             [
-                updatedDoctor.registration_no,
-                updatedDoctor.first_name,
-                updatedDoctor.last_name,
+                updatedDoctor.full_name,
                 updatedDoctor.gender,
-                updatedDoctor.cnic,
-                updatedDoctor.age,
-                updatedDoctor.father_name,
-                updatedDoctor.father_cnic,
-                updatedDoctor.post_office,
-                updatedDoctor.tehsil,
-                updatedDoctor.district,
+                updatedDoctor.contact_number,
+                updatedDoctor.email_address,
+                updatedDoctor.address,
+                updatedDoctor.specialization,
                 id
             ], (err, result) => {
                 if (err) reject(err);
@@ -106,15 +100,10 @@ async function updateDoctor(id, updatedDoctor) {
 
 async function deleteDoctor(id) {
     return new Promise((resolve, reject) => {
-        db.execute(`SELECT * FROM doctors WHERE id=?`, [id], (err, result) => {
-            if (result[0].is_admin == '1') resolve(false);
-            else {
-                db.execute(`DELETE FROM doctors WHERE id=?`, [id], (err, result) => {
-                    if (err) reject(err);
-                    if (result.affectedRows == 1) resolve(true);
-                    else resolve(false);
-                });
-            }
+        db.execute(`DELETE FROM doctors WHERE id=?`, [id], (err, result) => {
+            if (err) reject(err);
+            if (result.affectedRows == 1) resolve(true);
+            else resolve(false);
         });
     })
 }
