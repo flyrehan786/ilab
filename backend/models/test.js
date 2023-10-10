0605db7570055ca900b1a18b10d817eb4edc2857
Rehan Shah <flyrehan786@gmail.com>
2023-10-10 23:13:41 +0500

    working on test model.

---
+++ backend/models/test.js
@@ -0,0 +1,114 @@
+const Joi = require("joi");
+const db = require('../services/mysql').db;
+
+function validateTest(test) {
+    const schema = {
+        name: Joi.string()
+            .min(4)
+            .max(256)
+            .required(),
+        unit: Joi.string()
+            .min(4)
+            .max(256)
+            .required(),
+        male_reference_rage: Joi.string()
+            .min(1)
+            .max(1)
+            .required(),
+        male_reference_rage: Joi.string()
+            .min(6)
+            .max(256),
+        price: Joi.string()
+            .min(6)
+            .max(256)
+            .required(),
+        description: Joi.string()
+            .min(6)
+            .max(256)
+            .required()
+    };
+    return Joi.validate(test, schema);
+}
+
+async function findAll() {
+    return new Promise((resolve, reject) => {
+        db.execute((`SELECT * FROM tests`), [], (err, result) => {
+            if (err) reject(err);
+            if (result.length > 0) resolve(result);
+            else resolve([]);
+        })
+    })
+}
+
+async function saveTest(newTest) {
+    return new Promise((resolve, reject) => {
+        db.execute(`INSERT INTO patients VALUES(default, ?, ?, ?, ?, ?, ?, NOW(), NOW())`,
+            [
+                newTest.full_name,
+                newTest.date_of_birth,
+                newTest.gender,
+                newTest.contact_number,
+                newTest.email_address,
+                newTest.address,
+            ], (err, result) => {
+                if (err) reject(err);
+                db.execute(`SELECT id FROM patients WHERE id = LAST_INSERT_ID();`, (err, result) => {
+                    if (err) reject(err);
+                    if (result.length > 0) resolve(result[0].id);
+                    else resolve(null);
+                })
+            });
+    })
+}
+
+async function findTest(id) {
+    return new Promise((resolve, reject) => {
+        db.execute(`SELECT * FROM patients WHERE id=?`,
+            [
+                id
+            ], (err, result) => {
+                if (err) reject(err);
+                if (result.length > 0) resolve(result[0]);
+                else resolve(null);
+            });
+    })
+}
+
+async function updateTest(id, updatedTest) {
+    return new Promise((resolve, reject) => {
+        db.execute('Update patients SET full_name=?,date_of_birth=?, gender=?, contact_number=?, email_address=?, address=? WHERE id=?;',
+            [
+                updatedTest.full_name,
+                updatedTest.date_of_birth,
+                updatedTest.gender,
+                updatedTest.contact_number,
+                updatedTest.email_address,
+                updatedTest.address,
+                id
+            ], (err, result) => {
+                if (err) reject(err);
+                db.execute(`SELECT * FROM patients WHERE id = ${id};`, (err, result) => {
+                    if (err) reject(err);
+                    if (result.length > 0) resolve(result[0]);
+                    else resolve(null);
+                })
+            })
+    })
+}
+
+async function deleteTest(id) {
+    return new Promise((resolve, reject) => {
+        db.execute(`DELETE FROM patients WHERE id=?`, [id], (err, result) => {
+            if (err) reject(err);
+            if (result.affectedRows == 1) resolve(true);
+            else resolve(false);
+        });
+    })
+}
+
+exports.validate = validateTest;
+exports.findTest = findTest;
+exports.saveTest = saveTest;
+exports.findAll = findAll;
+exports.updateTest = updateTest;
+exports.deleteTest = deleteTest;
