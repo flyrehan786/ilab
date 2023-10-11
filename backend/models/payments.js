3b8b21f9e89e0f74ded20e033226f6da085d8340
Rehan Shah <flyrehan786@gmail.com>
2023-10-11 22:47:39 +0500

    implementing payments model.

--- backend/models/payments.js
+++ backend/models/payments.js
@@ -26,3 +26,84 @@ function validatePayment(payment) {
     };
     return Joi.validate(payment, schema);
 }
+
+async function findAll() {
+    return new Promise((resolve, reject) => {
+        db.execute((`SELECT * FROM payments`), [], (err, result) => {
+            if (err) reject(err);
+            if (result.length > 0) resolve(result);
+            else resolve([]);
+        })
+    })
+}
+
+async function savePayment(newPayment) {
+    return new Promise((resolve, reject) => {
+        db.execute(`INSERT INTO payments VALUES(default, ?, ?, ?, ?, ?, NOW(), NOW())`,
+            [
+                newPayment.patient_test_uuid,
+                newPayment.total_amount,
+                newPayment.total_discount,
+                newPayment.total_paid_amount,
+                newPayment.total_balance_amount
+            ], (err, result) => {
+                if (err) reject(err);
+                db.execute(`SELECT id FROM payments WHERE id = LAST_INSERT_ID();`, (err, result) => {
+                    if (err) reject(err);
+                    if (result.length > 0) resolve(result[0].id);
+                    else resolve(null);
+                })
+            });
+    })
+}
+
+async function findPayment(id) {
+    return new Promise((resolve, reject) => {
+        db.execute(`SELECT * FROM payments WHERE id=?`,
+            [
+                id
+            ], (err, result) => {
+                if (err) reject(err);
+                if (result.length > 0) resolve(result[0]);
+                else resolve(null);
+            });
+    })
+}
+
+async function updatePayment(id, updatedPayment) {
+    return new Promise((resolve, reject) => {
+        db.execute('Update payments SET patient_test_uuid=?,total_amount=?, total_discount=?, total_paid_amount=?, total_balance_amount=? updated_at=Now() WHERE id=?;',
+            [
+                updatedPayment.patient_test_uuid,
+                updatedPayment.total_amount,
+                updatedPayment.total_discount,
+                updatedPayment.total_paid_amount,
+                updatedPayment.total_balance_amount,
+                id
+            ], (err, result) => {
+                if (err) reject(err);
+                db.execute(`SELECT * FROM payments WHERE id = ${id};`, (err, result) => {
+                    if (err) reject(err);
+                    if (result.length > 0) resolve(result[0]);
+                    else resolve(null);
+                })
+            })
+    })
+}
+
+async function deletePayment(id) {
+    return new Promise((resolve, reject) => {
+        db.execute(`DELETE FROM payments WHERE id=?`, [id], (err, result) => {
+            if (err) reject(err);
+            if (result.affectedRows == 1) resolve(true);
+            else resolve(false);
+        });
+    })
+}
+
+exports.validate = validatePayment;
+exports.findPayment = findPayment;
+exports.savePayment = savePayment;
+exports.findAll = findAll;
+exports.updatePayment = updatePayment;
+exports.deletePayment = deletePayment;
