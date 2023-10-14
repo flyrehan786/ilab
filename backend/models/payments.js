435cbb8752b2e016929ee5f766cdb659f6cfedb9
Rehan Shah <flyrehan786@gmail.com>
2023-10-14 21:13:06 +0500

    ...

--- backend/models/payments.js
+++ backend/models/payments.js
@@ -49,7 +49,7 @@ async function savePayment(newPayment) {
             ], (err, result) => {
                 console.log(result);
                 if (err) reject(err);
-                db.execute(`SELECT id FROM payments WHERE id = ?;`, [ result?.insertId ], (err, result) => {
+                db.execute(`SELECT id FROM payments WHERE id = LAST_INSERT_ID();`, [ result?.insertId ], (err, result) => {
                     if (err) reject(err);
                     if (result.length > 0) resolve(result[0].id);
                     else resolve(null);
