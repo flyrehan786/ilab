359e769c45996157f593c250ecb66f1d9d744c5a
Rehan Shah <flyrehan786@gmail.com>
2023-10-15 00:17:08 +0500

    code refactoring.

--- backend/routes/patient-tests.js
+++ backend/routes/patient-tests.js
@@ -9,12 +9,20 @@ const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 
 router.get("", async (req, res) => {
-    const patientTests = await patientTestsModel.findAll();
-    patientTests.forEach(x => {
-        x.created_at = new Date(x.created_at).toLocaleString();
-        x.updated_at = new Date(x.updated_at).toLocaleString();
-    })
-    res.send(patientTests);
+    try {
+        const patientTests = await patientTestsModel.findAll();
+        const formattedPatientTests = patientTests.map(test => {
+          return {
+            ...test,
+            created_at: new Date(test.created_at).toLocaleString(),
+            updated_at: new Date(test.updated_at).toLocaleString(),
+          };
+        });
+        res.send(formattedPatientTests);
+      } catch (error) {
+        console.error('Error fetching patient tests:', error);
+        res.status(500).send('Internal Server Error');
+      }
 });
 
 router.post("", async (req, res) => {
