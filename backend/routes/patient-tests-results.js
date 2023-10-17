5d6561c970259dc7d0cfe727e52f34345b396fd9
Rehan Shah <flyrehan786@gmail.com>
2023-10-18 04:03:55 +0500

    fixing minor issue in patient-test-results api.

--- backend/routes/patient-tests-results.js
+++ backend/routes/patient-tests-results.js
@@ -10,52 +10,62 @@ router.get("", async (req, res) => {
 
 router.post("", async (req, res) => {
     // Request Data.
-    // const req.body = {
-    //     patient_test_uuid: '1697201760058',
-    //     test_results: [
-    //         {
-    //             test_id: 1,
-    //             result_value: 'adadasd',
-    //             remarks: '...'
-    //         },
-    //         {
-    //             test_id: 2,
-    //             result_value: 'hjgj',
-    //             remarks: '...'
-    //         },
-    //         {
-    //             test_id: 3,
-    //             result_value: 'hgjjdasd',
-    //             remarks: '...'
-    //         },
-    //         {
-    //             test_id: 4,
-    //             result_value: 'kjtvcd',
-    //             remarks: '...'
-    //         },
-    //     ]
-    // }
+    req.body = {
+        patient_test_uuid: '1697201760058',
+        test_results: [
+            {
+                test_id: 1,
+                result_value: 'adadasd',
+                remarks: '...'
+            },
+            {
+                test_id: 2,
+                result_value: 'hjgj',
+                remarks: '...'
+            },
+            {
+                test_id: 3,
+                result_value: 'hgjjdasd',
+                remarks: '...'
+            },
+            {
+                test_id: 4,
+                result_value: 'kjtvcd',
+                remarks: '...'
+            },
+        ]
+    }
     // Loop through Test Results from request data.
     const uuid = req.body.patient_test_uuid;
     const testResults = req.body.test_results;
     let queryResults = { patient_test_uuid: uuid, updateQueryResult: [] };
-    testResults.forEach(async tr => {
+    for (let t = 0; t < testResults.length; t++) {
+        const tr = testResults[t];
         const recordExist = await patientTestsResultsModel.findPatientTestsResultByTestIdAndUUID(uuid, tr.test_id);
         if (recordExist) {
-            console.log('updating.....');
             const queryResult = await patientTestsResultsModel.updatePatientTestsResultByTestIdAndUUID({
                 patient_tests_uuid: uuid,
                 test_id: tr.test_id,
                 result_value: tr.result_value,
                 remarks: tr.remarks
             });
-            queryResults.updateQueryResult.push({
-                payload: {
-                    test_id: tr.test_id,
-                    remarks: tr.remarks
-                },
-                queryResponse: queryResult
-            });
+            if (queryResult) {
+                queryResults.updateQueryResult.push({
+                    payload: {
+                        test_id: tr.test_id,
+                        remarks: tr.remarks,
+                        queryResponse: true
+                    },
+                });
+            } else {
+                queryResults.updateQueryResult.push({
+                    payload: {
+                        test_id: tr.test_id,
+                        remarks: tr.remarks,
+                        queryResponse: false
+                    },
+                });
+            }
         } else {
             const queryResult = await patientTestsResultsModel.savePatientTestResult({
                 patient_tests_uuid: uuid,
@@ -63,19 +73,30 @@ router.post("", async (req, res) => {
                 result_value: tr.result_value,
                 remarks: tr.remarks
             });
-            queryResults.updateQueryResult.push({
-                payload: {
-                    test_id: tr.test_id,
-                    remarks: tr.remarks
-                },
-                queryResponse: queryResult
-            });
+            if (queryResult) {
+                queryResults.updateQueryResult.push({
+                    payload: {
+                        test_id: tr.test_id,
+                        remarks: tr.remarks,
+                        queryResponse: true
+                    },
+                });
+            } else {
+                queryResults.updateQueryResult.push({
+                    payload: {
+                        test_id: tr.test_id,
+                        remarks: tr.remarks,
+                        queryResponse: false
+                    },
+                });
+            }
         }
-    });
+    }
     const patientStatusQueryResponse = await patientTestsModel.patientTestCompletedByUUID(uuid);
     res.send({
+        success: true,
         queryResults,
-        patientStatusQueryResponse
+        statusUpdated: patientStatusQueryResponse.length === req.body.test_results.length
     })
 });
 
