282aba53c63beee617a54504b654cd8ca8748f91
Rehan Shah <flyrehan786@gmail.com>
2023-10-17 15:42:35 +0500

    code refactoring.

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,13 @@
 const { check_connection } = require('./services/mysql');
 const express = require("express");
+const routes = require('./routes-config/routes');
+const cors = require('cors');
 
 const app = express();
-const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-
-const usersRoutes = require('./routes/users');
-const authRoute = require('./routes/auth');
-const patientsRoutes = require('./routes/patients');
-const doctorsRoutes = require('./routes/doctors');
-const testsRoutes = require('./routes/tests');
-const patientTestsRoutes = require('./routes/patient-tests');
-
-app.use('/api/auth', authRoute);
-app.use('/api/users', usersRoutes);
-app.use('/api/patients', patientsRoutes);
-app.use('/api/doctors', doctorsRoutes);
-app.use('/api/tests', testsRoutes);
-app.use('/api/tests', testsRoutes);
-app.use('/api/patient-tests', patientTestsRoutes);
+app.use('/', routes);
 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
