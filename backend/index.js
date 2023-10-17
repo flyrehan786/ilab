4ffa5a29887cdca6474ce4660d3d675c61652bf3
Rehan Shah <flyrehan786@gmail.com>
2023-10-18 04:53:32 +0500

    updating config for cluster mode.

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const routes = require('./routes-config/routes');
 const cors = require('cors');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-
-if (cluster.isMaster) {
-    // This is the master process
-    console.log(`Master ${process.pid} is running`);
-    // Fork workers for each CPU core
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
+const config = require('./config/default.json');
+const MYSQL = require('./services/mysql');
+if (config.CLUSTER_MODE == true) {
+    const cluster = require('cluster');
+    const numCPUs = require('os').cpus().length;
+    if (cluster.isMaster) {
+        console.log(`Master ${process.pid} is running`);
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Worker ${worker.process.pid} died`);
+        });
+    } else {
+        const app = express();
+        app.use(cors());
+        app.use(express.json());
+        app.use('/', routes);
+        const port = process.env.PORT || 4000;
+        app.listen(port, () => {
+            console.log(`Worker ${process.pid} is listening on port ${port}`);
+            console.log(`Listening on port ${port}...`)
+            MYSQL.check_connection();
+        });
     }
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-    });
 } else {
-    // This is a worker process
     const app = express();
     app.use(cors());
     app.use(express.json());
@@ -23,5 +34,7 @@ if (cluster.isMaster) {
     const port = process.env.PORT || 4000;
     app.listen(port, () => {
         console.log(`Worker ${process.pid} is listening on port ${port}`);
+        console.log(`Listening on port ${port}...`)
+        MYSQL.check_connection();
     });
 }
