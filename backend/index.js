0f5961e03f6e7e522e615ccce1c2b5ae17351a4b
Rehan Shah <flyrehan786@gmail.com>
2023-10-18 04:48:01 +0500

    implementing cluster mode implementation.

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,27 @@
-const MYSQL = require('./services/mysql');
-const express = require("express");
+const express = require('express');
 const routes = require('./routes-config/routes');
 const cors = require('cors');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/', routes);
-
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-    console.log(`Listening on port ${port}...`)
-    MYSQL.check_connection();
-});
-
-// const ZERO = require('./services/redis/database/redis-db');
-// ZERO.worker.
-// ZERO.worker.keys().then(keys => {
-//     console.log(keys);
-// })
-// ZERO.worker.save('key-001', { name: 'John', details: '...' });
-// ZERO.worker.keys().then(keys => {
-//     console.log(keys);
-// })
-
-module.exports = server;
+if (cluster.isMaster) {
+    // This is the master process
+    console.log(`Master ${process.pid} is running`);
+    // Fork workers for each CPU core
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    // This is a worker process
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use('/', routes);
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+        console.log(`Worker ${process.pid} is listening on port ${port}`);
+    });
+}
