64d3a0d44fa4affd6253423e6cf0a20b3f4d610d
Rehan Shah <flyrehan786@gmail.com>
2023-10-27 01:23:44 +0500

    code refactoring.

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ if (config.CLUSTER_MODE == true) {
     const numCPUs = require('os').cpus().length;
     if (cluster.isMaster) {
         console.log(`Master ${process.pid} is running`);
-        for (let i = 0; i < numCPUs; i++) {
-            cluster.fork();
-        }
+        for (let i = 0; i < numCPUs; i++) { cluster.fork() }
         cluster.on('exit', (worker, code, signal) => {
             console.log(`Worker ${worker.process.pid} died`);
         });
